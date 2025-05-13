ae241c899a779bab8999bdfd1b97aaeb0abcdd97
FX-Awarx <maxime.rb@orange.fr>
2025-05-13 11:51:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
+
 
 DATA_FILE = 'data.json'
 
